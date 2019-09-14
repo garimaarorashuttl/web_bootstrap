c2b273ec687b87000b27f2dafa67a00de34134d7
garimaarorashuttl <aroragarima0507@gmail.com>
2019-09-14 19:05:14 +0530

    Creating dist with minified content

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $("#mycarousel").carousel( { interval: 2000 } );
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children('span').hasClass('fa-pause')){
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children('span').removeClass('fa-pause');
+            $("#carouselButton").children('span').addClass('fa-play');
+        } else if ($("#carouselButton").children('span').hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children('span').removeClass('fa-play');
+            $("#carouselButton").children('span').addClass('fa-pause');
+        }
+    });
+    $("#reserveTableButton").click(function(){
+        $('#reserveTableModal').modal('toggle')
+    });
+    $("#cancelReserve").click(function(){
+        $('#reserveTableModal').modal('hide')
+    });
+    $("#cancelReserveCross").click(function(){
+        $('#reserveTableModal').modal('hide')
+    });
+    $("#loginButton").click(function(){
+        $('#loginModal').modal('toggle')
+    });
+    $("#cancelLogin").click(function(){
+        $('#loginModal').modal('hide')
+    });
+    $("#dismissCrossLogin").click(function(){
+        $('#loginModal').modal('hide')
+    });
+}
+);
+
